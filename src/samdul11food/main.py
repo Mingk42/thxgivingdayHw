ec524c1219b08e9a70785362d3e07d5170d648a3
Mingk42 <xoals123456t@gmail.com>
2024-09-13 10:13:15 +0900

    save ok

--- src/samdul11food/main.py
+++ src/samdul11food/main.py
@@ -13,9 +13,8 @@ def idx():
 @app.get("/food")
 def food(name:str):
     """
-    음식 이름 저장 API
-    # 시간을 저장
-    # 음식이름과 시간을 csv로 저장 -> code/data/food.csv
+    # 음식 이름 저장 API
+    ### 음식 이름과 시간을 csv로 저장 -> /code/data/food.csv
 
     Args:
      - name(str): 음식 이름
@@ -23,16 +22,16 @@ def food(name:str):
     Return
      - dict, 음식 이름과 저장 시간을 담은 딕셔너리
     """
-    
-    os.path.expanduser("~")
+    save_path=f"{os.path.dirname(os.path.abspath(__file__))}/data"
 
-    time="yyyy-mm-dd hh:mm:ss"
-    print(datetime.ctime())
+    os.makedirs(save_path,exist_ok=True)
 
-#    with open("~/code/data/food.csv","a") as f:
-#        f.write(name,time)
+    dt=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    with open(f"{save_path}/food.csv","a") as f:
+        f.write(f"{name},{dt}\n")
 
     return {
             "food":name, 
-            "time":time
+            "time":dt
             }
